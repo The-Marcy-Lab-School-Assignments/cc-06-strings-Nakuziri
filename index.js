8c920bc7ac50d685659fa8202774e6e0ce3637a8
Xavier Campos <xcampos@brooklynsteamcenter.org>
2024-10-10 11:06:15 -0400

    Finished the first two questions of the cc

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //write your code here
+const reverseString = (str) => {
+    let empty = ''
+    for(let i = str.length -1; i >= 0; i--) {
+       empty += str[i];
+    }
+    return empty;
+}
+
+console.log(reverseString('hello'));
+
+const reverseZigZagString = (str) => {
+    let empty = ''
+    for(let i = str.length -1; i >= 0; i--) {
+       if(i % 2 === 0){
+        empty += str[i].toUpperCase();
+        } else {
+            empty += str[i];
+        }
+    }
+    return empty;
+}
+
+console.log(reverseZigZagString('hello'));
